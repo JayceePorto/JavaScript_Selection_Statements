9556efbbc23e0406e6ed866cf7e4d156ca7f527e
Jaycee Porto <jaycee.porto@outlook.com>
2022-06-09 21:09:59 -0500

    Completed Exercise

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ console.log(
 );
 
 // Exercise 1
-const num = 16;
+const num = 22;
 
-let userInput = window.prompt("Guess a number between 1 and 100:");
+let userInput = window.prompt("Guess a number");
 
 if (userInput < num) {
   alert("Too low");
@@ -18,15 +18,13 @@ if (userInput < num) {
 
 // Exercise 2
 let birthMonth = window
-  .prompt('Enter your birth month (e.g. "December"):')
-  .toLowerCase();
+  .prompt("What is your birth month?")
 
-switch (birthMonth) {
-  case "december":
-  case "january":
+switch (birthMonth.toLowerCase()) {
+  case "december": 
+  case "january": 
   case "february":
-  case "march":
-    alert("You were born in Winter.");
+    console.log("You were born in Winter.");
     break;
   case "april":
   case "may":
@@ -45,3 +43,102 @@ switch (birthMonth) {
   default:
     alert("You entered a invalid month.");
 }
+
+//Exercise 3
+
+let typeId = "01";
+let colorId = "PU";
+let sizeId = "L";
+
+let type = "";
+let color = "";
+let size = "";
+
+if (typeId == "01") {
+  type = "Tank top";
+} else if (typeId == "02") {
+  type = "T-Shirt";
+} else if (typeId == "03") {
+  type = "Long Sleeve";
+} else if (typeId == "04") {
+  type == "Sweat Shirt";
+} else {
+  type = "Other";
+}
+
+switch (typeId) {
+  case "01":
+    type = "Tank top";
+    break;
+  case "02":
+    type = "T-shirt";
+    break;
+  case "03":
+    type = "Long Sleeve";
+    break;
+  case "04":
+    type = "Sweat Shirt";
+    break;
+  default:
+    type ="Other";
+}
+
+if (colorId == "BL") {
+  color = "Black";
+} else if (colorId == "BU") {
+  color = "Blue";
+} else if (colorId == "RD") {
+  color = "Red";
+} else if (colorId == "PU") {
+  color = "Purple";
+} else {
+  color = "White";
+}
+
+switch (colorId) {
+  case "BL":
+    color = "Black";
+    break;
+  case "BU":
+    color = "Blue";
+    break;
+  case "RD":
+    color = "Red";
+    break;
+  case "PU":
+    color = "Purple";
+    break;
+  default:
+    type ="Other";
+}
+
+if (sizeId == "S") {
+  size = "Small";
+} else if (sizeId == "M") {
+  size = "Medium";
+} else if (sizeId == "L") {
+  size = "Large";
+} else if (sizeId == "XL") {
+  size = "Extra Large";
+} else {
+  size = "One Size Fits All";
+}
+
+switch (sizeId) {
+  case "S":
+    size = "Small";
+    break;
+  case "M":
+    size = "Medium";
+    break;
+  case "L":
+    size = "Large";
+    break;
+  case "XL":
+    size = "Extra Large";
+    break;
+  default:
+    type ="Other";
+}
+
+console.log(`Product: ${size} ${color} ${type}`);
